23df313711fa1d680dbe79b48409647221b59d71
Stardust <stardustdev@outlook.com>
2020-09-21 11:56:05 -0400

    Add blog manage

---
+++ build/precache-manifest.89d1e3acd06a0a9f4680b91e09d121a1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4a0e0236758d9359b7d35e97d42da8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "212f9b4ac7854811aa08",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d4e1ac435b777988ade1",
+    "url": "/static/css/main.15314033.chunk.css"
+  },
+  {
+    "revision": "212f9b4ac7854811aa08",
+    "url": "/static/js/2.16ba4151.chunk.js"
+  },
+  {
+    "revision": "d98d30b660f3a25cb218065d04fbab6e",
+    "url": "/static/js/2.16ba4151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e1ac435b777988ade1",
+    "url": "/static/js/main.2c8ff152.chunk.js"
+  },
+  {
+    "revision": "483baf904187c285b211",
+    "url": "/static/js/runtime-main.ff41a14b.js"
+  },
+  {
+    "revision": "f2b61601f4f10c75136a9db83993596c",
+    "url": "/static/media/avatar.f2b61601.png"
+  },
+  {
+    "revision": "9b0609300893f9f5ec7066cdc026b23d",
+    "url": "/static/media/coming-soon.9b060930.png"
+  },
+  {
+    "revision": "8d212731dacb5a178980d0df40d57461",
+    "url": "/static/media/green-check.8d212731.svg"
+  },
+  {
+    "revision": "a214c34da1ea1e433ca2aa2ac293f7c6",
+    "url": "/static/media/info_icon.a214c34d.svg"
+  },
+  {
+    "revision": "8f87bdb9abc76eba0baf367020d4f9d1",
+    "url": "/static/media/red-stop.8f87bdb9.svg"
+  },
+  {
+    "revision": "ba7f8db39fd2737eaf193198fb38f4a5",
+    "url": "/static/media/warning-icon.ba7f8db3.svg"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ToastProvider } from 'react-toast-notifications';
+
+import CustomToast from 'components/CustomToast';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import App from './App';
+import { store, persistor } from './redux/store';
+import './index.scss';
+import * as serviceWorker from './serviceWorker';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <ToastProvider
+        placement="bottom-center"
+        autoDismissTimeout={4000}
+        autoDismiss={true}
+        components={{ Toast: CustomToast }}
+      >
+        <App />
+      </ToastProvider>
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root'),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
